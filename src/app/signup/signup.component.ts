fbc84109f7291adc5cd0623af7b1cc1cd3ed864d
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-25 23:01:01 +0530

    todo api

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
   name=""
   dob=""
   phone=""
@@ -25,6 +26,11 @@ export class SignupComponent implements OnInit {
       "password":this.password
     }
     console.log(data)
+    this.myapi.addSignup(data).subscribe(
+      (res)=>{
+        alert("successfully added")
+      }
+    )
     this.name=""
     this.dob=""
     this.phone=""
